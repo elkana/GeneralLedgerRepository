c35d16f69e591c522baf4568118b88e59a4ed669
andirhl13 <andirhl13@gmail.com>
2024-07-16 16:11:56 +0700

    Update Acmaster by Andi Rhl

--- src/main/java/com/GeneralLedger/Repository/IGlAcMasterRepository.java
+++ src/main/java/com/GeneralLedger/Repository/IGlAcMasterRepository.java
@@ -15,6 +15,18 @@ public interface IGlAcMasterRepository extends JpaRepository<GlAcMaster, String>
 	Optional<GlAcMaster> findByglamaccode(String code);
 	
 	List<GlAcMaster> findByglamacstatus(String status);
+	
+	List<GlAcMaster> findByglamaccodeContaining(String code);
+	
+	List<GlAcMaster> findByglamacstatusAndGlamaccodeContaining(String status, String code);
+	
+	List<GlAcMaster> findByglamacdescContaining(String desc);
+	
+	List<GlAcMaster> findByglamacstatusAndGlamacdescContaining(String status, String desc);
+	
+	List<GlAcMaster> findByglamacdesc2Containing(String desc2);
+	
+	List<GlAcMaster> findByglamacstatusAndGlamacdesc2Containing(String status, String desc2);
     
 	@Query("select s.glamacdesc from GlAcMaster s where s.glamaccode=?1")
 	public String getMstAccDesc(String code);
